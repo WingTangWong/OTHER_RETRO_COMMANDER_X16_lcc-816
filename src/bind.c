0e4d76607ee6461cb9b8c08ed19ad24b75efe7fe
Kelvin W Sherlock <kelvin@ksherlock.com>
2013-09-27 21:42:51 -0400

    iigs symbolic

--- src/bind.c
+++ src/bind.c
@@ -7,6 +7,7 @@ xx(sparc/sun,    sparcIR) \
 xx(sparc/solaris,solarisIR) \
 xx(x86/win32,    x86IR) \
 xx(x86/linux,    x86linuxIR) \
+xx(symbolic/iigs,symbolic16IR) \
 xx(symbolic/osf, symbolic64IR) \
 xx(symbolic/irix,symbolicIR) \
 xx(symbolic,     symbolicIR) \
