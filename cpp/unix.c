ec97dcba4f2e502a9f88a76af018a8e11f573dc7
Kelvin Sherlock <ksherlock@gmail.com>
2015-04-15 11:24:20 -0400

    clang/os x define memmove with a macro, so remove it to prevent compile errors.

--- cpp/unix.c
+++ cpp/unix.c
@@ -89,7 +89,7 @@ setup(int argc, char **argv)
 }
 
 
-
+#ifndef __clang__
 /* memmove is defined here because some vendors don't provide it at
    all and others do a terrible job (like calling malloc) */
 void *
@@ -98,7 +98,7 @@ memmove(void *dp, const void *sp, size_t n)
 	unsigned char *cdp, *csp;
 
 	if (n<=0)
-		return 0;
+		return dp;
 	cdp = dp;
 	csp = (unsigned char *)sp;
 	if (cdp < csp) {
@@ -112,5 +112,6 @@ memmove(void *dp, const void *sp, size_t n)
 			*--cdp = *--csp;
 		} while (--n);
 	}
-	return 0;
+	return dp;
 }
+#endif
