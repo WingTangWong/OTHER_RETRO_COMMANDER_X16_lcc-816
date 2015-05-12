c989531b16cddfaad6bb3efe997f13abf6e3605d
Kelvin Sherlock <ksherlock@gmail.com>
2015-05-12 11:08:39 -0400

    nicer 816 bindings

--- src/bind.c
+++ src/bind.c
@@ -14,6 +14,8 @@ xx(symbolic,     symbolicIR) \
 xx(bytecode,     bytecodeIR) \
 xx(msil,         msilIR) \
 xx(msil/win32,   msilIR) \
+xx(65816,        wdc65816IR) \
+xx(65816/iigs,   wdc65816IR) \
 xx(wdc65816/iigs,wdc65816IR) \
 xx(null,         nullIR)
 
