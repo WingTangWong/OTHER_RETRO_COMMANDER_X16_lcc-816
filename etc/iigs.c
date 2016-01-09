133955dc817ceb7469e695378d71edb59f90eccf
Kelvin Sherlock <ksherlock@gmail.com>
2016-01-08 22:11:56 -0500

    clean up iigs lcc driver code (paths and such)

--- etc/iigs.c
+++ etc/iigs.c
@@ -5,22 +5,33 @@
 static char rcsid[] = "$Id$";
 
 #ifndef LCCDIR
-#define LCCDIR "/usr/local/lib/lcc-816/"
+#define LCCDIR "/usr/local/share/lcc-816/"
 #endif
 
 // -DGNO 
-char *suffixes[] = { ".c", ".i", ".s", ".o", ".out", 0 };
+/*
+	$1 is extra arguments (from the command line)
+	$2 is the input file
+	$3 is the output file.
+*/
+
+char *suffixes[] = { ".c", ".i", ".s", ".omf", ".out", 0 };
 char inputs[256] = "";
-char *cpp[] = { LCCDIR "cpp",
+char *cpp[] = { LCCDIR "bin/cpp",
 	"-D__STDC__=1", "-D__appleiigs__", "-D__65816__",
 	"$1", "$2", "$3", 0 };
 
-char *include[] = { "-I" LCCDIR "include", "-I" LCCDIR "toolbox", 0 };
-char *com[] = { LCCDIR "rcc", "-target=wdc65816/iigs",
+char *include[] = { "-I" LCCDIR "include", "-I" LCCDIR "include/toolbox", 0 };
+char *com[] = { LCCDIR "bin/rcc", "-target=wdc65816/iigs",
 	"$1", "$2", "$3", 0 };
-char *as[] = { LCCDIR "as", "-o", "$3", "$1", "$2", 0 };
-char *ld[] = { LCCDIR "ld", "-o", "$3", "$1", 
-	LCCDIR "lib/startup.o", "-L" LCCDIR "lib/", 0 };
+char *as[] = { LCCDIR "bin/asm-816", "-o", "$3", "$1", "$2", 0 };
+char *ld[] = { LCCDIR "bin/ld-816", 
+	"-o", "$3", 
+	"$1", 
+	LCCDIR "lib/crt.omf", 
+	"-L" LCCDIR "lib/", 
+	"$2", 
+	0 };
 
 extern char *concat(char *, char *);
 
